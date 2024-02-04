219ec5d18c8f1ce217d436672866fdc94b0833d6
MatthieuOByrne <151106237+MatthieuOByrne@users.noreply.github.com>
2024-02-03 16:43:13 -0800

    first commit

---
+++ Backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port: 5555,
+    CONNECTION_STRING: 'mongodb+srv://matthieuobyrne2009:matthieu@Info.lndqojf.mongodb.net/',
+    jwtSecret: "vsdvfsdvfvdfvfbdvfdvdgbvgvgvgdv",
+    jwtTokenDuration: 30 * 24 * 60 * 60
+};
+J
